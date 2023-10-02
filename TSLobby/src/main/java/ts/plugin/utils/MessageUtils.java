b10d196e829c42d6c74250df407955a54e9f7934
TeleMinecraft <teleminecraft.s@gmail.com>
2023-10-02 13:44:55 -0300

    Delete TSLobby directory

--- TSLobby/src/main/java/ts/plugin/utils/MessageUtils.java
+++
@@ -1,9 +0,0 @@
-package ts.plugin.utils;
-
-import org.bukkit.ChatColor;
-
-public class MessageUtils {
-    public static String getColoredMessage(String msg) {
-        return ChatColor.translateAlternateColorCodes('&', msg);
-    }
-}
